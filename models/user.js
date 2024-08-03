a408fdc73105b5f067d86ad008806f2aa28a09c2
FelixMR <felixmcneillrutter@gmail.com>
2024-08-03 15:35:37 +0100

    Added User.js model including data protection features

--- models/user.js
+++ models/user.js
@@ -1,1 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {type: string, required: true, unique: true },
+    password: {type: string, required: true },
+    email: {type: string, required: true },
+    listings: [{type: mongoose.Schema.Types.ObjectId, ref: "Listing"}],
+});
+
+userSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        delete returnedObject.password;
+        delete returnedObject.email;
+    }
+ });
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
