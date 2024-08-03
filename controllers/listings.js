90434efd0b7a90113f46a799cbb197258f4ef614
Andy Heggs <Andy@Heggs.net>
2024-08-03 15:38:21 +0100

    postman-setup-including-middleware-and-test-controller files

--- controllers/listings.js
+++ controllers/listings.js
@@ -2,3 +2,5 @@ const express = require('express')
 const verifyToken = require('../middleware/verify-token.js')
 const Listing = require('../models/listing.js')
 const router = express.Router()
+
+
--- middleware/verify-token.js
+++ middleware/verify-token.js
@@ -1,1 +1,23 @@
 const jwt = require('jsonwebtoken');
+
+function verifyToken(req, res, next) {
+
+  try {
+    // split JWT to aquire payload 
+    const token = req.headers.authorization.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // JWT payload to req.user
+    req.user = decoded;
+
+    //invoke middleware
+    next();
+
+  } catch (error) {
+
+    // handle errors
+    res.status(401).json({ error: 'Invalid authorization token.' });
+  }
+}
+
+module.exports = verifyToken;
