93d82004928cc0ba12428c05ff58bf2d56614d14
FelixMR <felixmcneillrutter@gmail.com>
2024-08-03 17:16:16 +0100

    Added error handeling

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -5,10 +5,15 @@ const verifyToken = require('../middleware/verify-token');
 
 // Create User Route
 router.post("/", async (req, res) => {
-    // Check data is valid
+    try {
+        // Check data is valid
     // Hash Password
     // Create User
     // Reurn Succsess codewith token
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: error.message});
+    }
 });
 
 module.exports = router;
