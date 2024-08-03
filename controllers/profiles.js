53c8d2155c598e5cb597f532d8f1b2378e987e13
FelixMR <felixmcneillrutter@gmail.com>
2024-08-03 17:14:10 +0100

    Added template for route structure

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -2,3 +2,13 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const verifyToken = require('../middleware/verify-token');
+
+// Create User Route
+router.post("/", async (req, res) => {
+    // Check data is valid
+    // Hash Password
+    // Create User
+    // Reurn Succsess codewith token
+});
+
+module.exports = router;
