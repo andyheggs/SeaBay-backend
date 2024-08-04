aae2281c2c6a5f81ee311c14c1f7c4bdbd85eab7
FelixMR <felixmcneillrutter@gmail.com>
2024-08-04 12:38:20 +0100

    Updated lexical standards

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,24 +1,30 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../models/user');
-const verifyToken = require('../middleware/verify-token');
-const bcrypt = require('bcryptjs');
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+const jwt = require("jsonwebtoken")
+const verifyToken = require('../middleware/verify-token')
+const bcrypt = require('bcryptjs')
+const dotenv = require('dotenv').config()
 
-// Create User Route
+// Creates User models 
 router.post("/signup", async (req, res) => {
     try {
+        // Hashes incoming password
         const hashedPassword = bcrypt.hashSync(req.body.password, 12)
+        // Creates new user using information from req.body and hashedPassword
      const newUser = await User.create({
         username: req.body.username, 
         password: hashedPassword,
         email: req.body.email
-     });
-    // Create User
-    // Reurn Succsess codewith token
+     })
+    //  Creates the new token
+     const token = jwt.sign({username: newUser.username, _id : newUser._id}, process.env.TOKENSECRET)
+    //  Returns both the created user and the token for that user 
+    res.status(200).json({newUser, token})
     } catch (error) {
-        console.log(error);
-        res.status(500).json({error: error.message});
+        console.log(error)
+        res.status(500).json({error: error.message})
     }
-});
+})
 
-module.exports = router;
+module.exports = router
