7af49ab495cedc6f7eb045a9e4243558d5e5dfbd
FelixMR <felixmcneillrutter@gmail.com>
2024-08-05 10:17:41 +0100

    Added signin template

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -27,4 +27,13 @@ router.post("/signup", async (req, res) => {
     }
 })
 
+router.post("/signin", async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error: error.message})
+    }
+})
+
 module.exports = router
