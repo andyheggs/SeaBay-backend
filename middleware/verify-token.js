368b6bd74e8cdb92c0c69c4d916fc2a723d8c0ba
FelixMR <felixmcneillrutter@gmail.com>
2024-08-07 14:42:48 +0100

    Added correct auth middleware

--- middleware/verify-token.js
+++ middleware/verify-token.js
@@ -9,7 +9,7 @@ function verifyToken(req, res, next) {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       // Assign decoded payload to req.user
-      req.user = decoded;
+      req.user = decoded.user;
 
       // Call next() to invoke the next middleware function
       next();
