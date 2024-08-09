159b6f727cc2058f419b4cb0041e660423ae1fee
Felix McNeill Rutter <142592359+MOTLWC@users.noreply.github.com>
2024-08-09 16:13:27 +0100

    Update api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,37 +1,33 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const mongoose = require('mongoose');
-const express = require('express');
-const serverless = require('serverless-http');
-const cors = require('cors');
-const testJWTRouter = require('../controllers/test-jwt');
-const profilesRouter = require('../controllers/profiles');
-const listingsRouter = require('../controllers/listings');
-const offersRouter = require('../controllers/offers');
+const serverless = require('serverless-http')
+
+const dotenv = require('dotenv')
+dotenv.config()
+const cors = require('cors')
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const testJWTRouter = require('../../controllers/test-jwt')
+const usersRouter = require('../../controllers/users')
+const profilesRouter = require('../../controllers/profiles')
+const listingsRouter = require('../../controllers/listings.js')
+const offersRouter = require('../../controllers/offers.js')
 const morgan = require('morgan');
 
-// Initialize Express app
-const app = express();
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGODB_URI)
 
 mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
 });
-
-// Middleware
-app.use(morgan('dev'));
+app.use(morgan("dev"))
 app.use(cors());
-app.use(express.json());
+app.use(express.json())
+// Morgan for logging HTTP requests
+app.use(morgan('dev'));
 
-// Routes
-app.use('/.netlify/functions/api/test-jwt', testJWTRouter);
-app.use('/.netlify/functions/api/profiles', profilesRouter);
-app.use('/.netlify/functions/api/listings', listingsRouter);
-app.use('/.netlify/functions/api/offers', offersRouter);
+app.use('/test-jwt', testJWTRouter)
+//app.use('/users', usersRouter)
+app.use('/profiles', profilesRouter)
+app.use('/listings', listingsRouter)
+app.use('/offers', offersRouter)
 
-// Export the serverless function
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app)
