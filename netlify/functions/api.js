e499d88ac31e9fee609a0b2c1f43214e4e19ca2a
Andy Heggs <Andy@Heggs.net>
2024-08-09 16:12:40 +0100

    updatetd netlify

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,48 +1,10 @@
-const dotenv = require('dotenv');
-dotenv.config();
-const mongoose = require('mongoose');
-const express = require('express');
-const serverless = require('serverless-http');
-const cors = require('cors');
-const testJWTRouter = require('../controllers/test-jwt');
-const profilesRouter = require('../controllers/profiles');
-const listingsRouter = require('../controllers/listings');
-const offersRouter = require('../controllers/offers');
-const morgan = require('morgan');
-
-// Initialize Express app
-const app = express();
-
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
-
-// Middleware
-app.use(morgan('dev'));
-app.use(cors());
-app.use(express.json());
-
-// Routes
-app.use('/.netlify/functions/api/test-jwt', testJWTRouter);
-app.use('/.netlify/functions/api/profiles', profilesRouter);
-app.use('/.netlify/functions/api/listings', listingsRouter);
-app.use('/.netlify/functions/api/offers', offersRouter);
-
-// Export the serverless function
-module.exports.handler = serverless(app);
-
 const serverless = require('serverless-http')
 
 const dotenv = require('dotenv')
 dotenv.config()
 const cors = require('cors')
 const express = require('express')
-const app = express()
+
 const mongoose = require('mongoose')
 const testJWTRouter = require('../../controllers/test-jwt')
 const usersRouter = require('../../controllers/users')
